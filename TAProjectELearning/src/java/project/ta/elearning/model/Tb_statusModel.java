60f68490e89b6f984bd9787f451c2c16daea9255
Nurikhsan <ikhsanhikari29@gmail.com>
2018-03-30 15:48:53 +0700

    merging

---
+++ TAProjectELearning/src/java/project/ta/elearning/model/Tb_statusModel.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.ta.elearning.model;
+
+/**
+ *
+ * @author Sou
+ */
+public class Tb_statusModel {
+
+    private String nama;
+    private Integer status;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+}
